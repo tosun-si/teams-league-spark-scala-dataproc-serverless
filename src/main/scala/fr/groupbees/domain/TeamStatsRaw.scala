69109e39c6f9bb222584796f048fee07e8b59164
Mazlum TOSUN <mazlum.tosun@gmail.com>
2023-08-08 01:20:11 +0200

    Remove useless validator on Raw data and add equals on jvmopts option.

--- src/main/scala/fr/groupbees/domain/TeamStatsRaw.scala
+++ src/main/scala/fr/groupbees/domain/TeamStatsRaw.scala
@@ -1,23 +1,6 @@
 package fr.groupbees.domain
 
-import TeamStatsRaw.TEAM_EMPTY_ERROR_MESSAGE
-import fr.groupbees.domain.exception.TeamStatsRawValidatorException
-
-import java.util.Objects
-
 case class TeamStatsRaw(teamName: String,
                         teamScore: Long,
                         scorers: List[TeamScorerRaw])
-  extends Serializable {
-
-  def validateFields(): TeamStatsRaw = {
-    if (Objects.isNull(teamName) || teamName == "") {
-      throw new TeamStatsRawValidatorException(TEAM_EMPTY_ERROR_MESSAGE)
-    }
-    this
-  }
-}
-
-object TeamStatsRaw {
-  private val TEAM_EMPTY_ERROR_MESSAGE = "Team name cannot be null or empty"
-}
+  extends Serializable
